317d018c5e7734ec9123db80fc1a1b375e6416a6
yuliusjati <54299598+yuliusjati@users.noreply.github.com>
2020-03-09 18:39:29 +0700

    Add files via upload

---
+++ file3.php
@@ -0,0 +1,13 @@
+<?php
+$nama = "Firdausa";
+$sekolah = "SMK Telkom Malang";
+$umur = 17;
+
+echo "Halo namaku $nama usiaku $umur tahun";
+echo "<br>";
+echo "Aku sekolah di $sekolah";
+echo "<br>";
+echo 'Halo namaku '.$nama.' usiaku '.$umur.' tahun';
+echo "<br>";
+echo 'Aku sekolah di '.$sekolah;
+?>
